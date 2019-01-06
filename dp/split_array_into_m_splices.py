e4b3239527d65ad474961e4241b1f9793653b2ec
lturing <1058385944@qq.com>
2019-01-06 14:42:12 +0800

    Create split_array_into_m_splices.py

---
+++ dp/split_array_into_m_splices.py
@@ -0,0 +1,76 @@
+'''
+for Segmental CRF
+SCRF is a variant of the linear-chain CRF model where each output token corresponds to a segment of input tokens instead of a single input instance.
+[multitask learning with ctc and segmental crf for speech recognition](https://arxiv.org/pdf/1702.06378.pdf)
+'''
+import copy 
+
+# split an array of lenght n into m splices
+'''
+n = input()
+m = input()
+'''
+
+n = 10
+m = 4
+
+assert n >= m, "n must be greater than m"
+
+f = {}
+
+def traverse_split(n=n, m=m):
+    if (n, m) not in f:
+        f[n, m] = 0
+    if m == 1:
+        f[n, m] = 1
+        return 
+    for i in range(n-1, max(m-2, 0), -1):
+        if (i, m-1) not in f:
+            traverse_split(i, m-1)
+        f[n, m] += f[i, m-1]
+
+'''
+traverse_split()
+print(f[n, m])
+'''
+
+f = {}
+def traverse_split_rec(n=n, m=m):
+    if (n, m) not in f:
+        f[n, m] = []
+    if m == 1:
+        f[n, m] = [n]
+        return 
+    for i in range(n-1, max(m-2, 0), -1):
+        if (i, m-1) not in f:
+            traverse_split_rec(i, m-1)
+        tmp = copy.deepcopy(f[i, m-1])
+
+        if type(tmp[0]) == list:
+            for item in tmp:
+                item.append(n)
+                f[n, m].append(item)
+        else:
+            tmp.append(n)
+            f[n, m].append(tmp)
+
+traverse_split_rec()
+ret = f[n, m]
+ret = sorted(ret, key=lambda asd:([asd[i] for i in range(len(asd))]), reverse=True)
+
+
+'''
+for item in ret:
+    tmp = [str(i+1) for i in range(n)]
+    for j in range(m-2, -1, -1):
+        tmp.insert(item[j], ',')
+    tmp = ''.join(tmp)
+    print(tmp)
+'''
+
+
+for item in ret:
+    tmp = ''.join([str(i+1) for i in range(n)])
+    for j in range(m-2, -1, -1):
+        tmp = tmp[:item[j]] + ',' + tmp[item[j]:]
+    print(tmp)
